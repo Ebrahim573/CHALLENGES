1dce56fa2a4b4aa1b0e49316f01651ac5a675ab0
Ebrahim573 <101477670+Ebrahim573@users.noreply.github.com>
2022-07-05 22:44:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ app.post('/submit', (req, res) => {
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'diwalirwisher@gmail.com',
-            pass: 'asdfghjkloqwertyuiop'
+            user: 'newyearwishmaster@gmail.com',
+            pass: 'iukqdonsjxlwwxdp'
         }
     });
 
     var mailOptions = {
-        from: 'diwalirwisher@gmail.com',
+        from: 'newyearwishmaster@gmail.com',
         to: req.body.email,
         subject: 'Happy Diwali',
         text: ` Hi ${req.body.wname}, ${req.body.uname} wishes you a happy and safe Diwali.`,
